9daeedb5dc6e061a37e13a07356b065bfb293828
GrishkaYesenin <grigorevaesenia@gmail.com>
2022-07-30 17:06:00 +0300

    Images are rendering on the site

--- main/urls.py
+++ main/urls.py
@@ -6,10 +6,4 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('add_book', views.add_book, name='add_book')
-]
-
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT
-    )
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
