24a13a7b27c6492af5f041b091d809a70a6b9451
GrishkaYesenin <grigorevaesenia@gmail.ccom>
2022-08-28 11:12:12 +0300

    Remove field 'booked'

--- main/models.py
+++ main/models.py
@@ -4,7 +4,6 @@ class Book(models.Model):
     name = models.CharField('Название', max_length=100)
     writer = models.CharField('Автор', max_length=50)
     img = models.ImageField('img',  )
-    booked = models.BooleanField('Бронь', default=False)
     datetime = models.DateTimeField('Время', auto_now=True)
 
 def __str__(self):
