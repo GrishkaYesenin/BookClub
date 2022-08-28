25a3556129f313f973328bea9ef399376ee91367
GrishkaYesenin <grigorevaesenia@gmail.ccom>
2022-08-28 11:11:50 +0300

    Add display links and searching fields on admin panel

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Book
 
-admin.site.register(Book)
-# Register your models here.
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('name', 'writer', 'datetime')
+    list_display_links = ('name',)
+    search_fields = ('name', 'writer', 'datetime')
+
+
+admin.site.register(Book, BookAdmin)
